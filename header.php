2bc0d37800a61d6cf94a5ddf581352dba0669487
yuka0258 <yuka_no_two@yahoo.co.jp>
2026-03-19 11:42:33 +0900

    Delete header.php

--- header.php
+++
@@ -1,8 +0,0 @@
-<h1>Yuka Takeuchi Portfolio</h1>
-        <nav id="g-nav">
-            <ul class="menu">
-                <li>
-                    <a href="index.html">WORK</a>
-                </li>
-            </ul>
-        </nav>
